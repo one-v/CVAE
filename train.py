dae2dcb9db40a8a886b5c785156265e75e0355a2
one-v <948419253@qq.com>
2026-02-01 17:29:18 +0800

    [2026.2.1] 初步适配光纤CVAE代码 具体流程还未优化 细节过程还未优化 训练过程还未可视化

--- train.py
+++ train.py
@@ -6,9 +6,9 @@ import pandas as pd
 import seaborn as sns
 import matplotlib.pyplot as plt
 from torchvision import transforms
-from torchvision.datasets import MNIST
 from torch.utils.data import DataLoader
 from collections import defaultdict
+from utils import create_dataset
 
 from models import VAE
 
@@ -25,26 +25,26 @@ def main(args):
 
     ts = time.time()
 
-    dataset = MNIST(
-        root='data', train=True, transform=transforms.ToTensor(),
-        download=True)
+    # 1、创建数据集，获取数据集的输入特征维度和输出标签维度
+    train_dataset, test_dataset, input_dim, output_dim = create_dataset()
     data_loader = DataLoader(
-        dataset=dataset, batch_size=args.batch_size, shuffle=True)
+        dataset=train_dataset, batch_size=args.batch_size, shuffle=True)
 
     def loss_fn(recon_x, x, mean, log_var):
-        BCE = torch.nn.functional.binary_cross_entropy(
-            recon_x.view(-1, 28 * 28), x.view(-1, 28 * 28), reduction='sum')
+        # 使用 Smooth L1  + KLD 作为最终损失函数
+        SmoothL1 = torch.nn.functional.smooth_l1_loss(recon_x, x, reduction='sum')
         KLD = -0.5 * torch.sum(1 + log_var - mean.pow(2) - log_var.exp())
 
-        return (BCE + KLD) / x.size(0)
+        return (SmoothL1 + KLD) / x.size(0)
 
     vae = VAE(
         encoder_layer_sizes=args.encoder_layer_sizes,
         latent_size=args.latent_size,
         decoder_layer_sizes=args.decoder_layer_sizes,
         conditional=args.conditional,
-        num_labels=10 if args.conditional else 0).to(device)
+        num_labels=6 if args.conditional else 0).to(device)
 
+    # 2、选择 Adam 优化器
     optimizer = torch.optim.Adam(vae.parameters(), lr=args.learning_rate)
 
     logs = defaultdict(list)
@@ -62,62 +62,26 @@ def main(args):
             else:
                 recon_x, mean, log_var, z = vae(x)
 
-            for i, yi in enumerate(y):
-                id = len(tracker_epoch)
-                tracker_epoch[id]['x'] = z[i, 0].item()
-                tracker_epoch[id]['y'] = z[i, 1].item()
-                tracker_epoch[id]['label'] = yi.item()
+            # 用于潜在空间可视化，可以先注释掉
+            # for i, yi in enumerate(y):
+            #     id = len(tracker_epoch)
+            #     tracker_epoch[id]['x'] = z[i, 0].item()
+            #     tracker_epoch[id]['y'] = z[i, 1].item()
+            #     tracker_epoch[id]['label'] = yi[0].item()
 
             loss = loss_fn(recon_x, x, mean, log_var)
 
+            # 梯度清零、反向传播、参数更新
             optimizer.zero_grad()
             loss.backward()
             optimizer.step()
 
             logs['loss'].append(loss.item())
 
-            if iteration % args.print_every == 0 or iteration == len(data_loader) - 1:
-                print("Epoch {:02d}/{:02d} Batch {:04d}/{:d}, Loss {:9.4f}".format(
-                    epoch, args.epochs, iteration, len(data_loader) - 1, loss.item()))
-
-                if args.conditional:
-                    c = torch.arange(0, 10).long().unsqueeze(1).to(device)
-                    z = torch.randn([c.size(0), args.latent_size]).to(device)
-                    x = vae.inference(z, c=c)
-                else:
-                    z = torch.randn([10, args.latent_size]).to(device)
-                    x = vae.inference(z)
-
-                plt.figure()
-                plt.figure(figsize=(5, 10))
-                for p in range(10):
-                    plt.subplot(5, 2, p + 1)
-                    if args.conditional:
-                        plt.text(
-                            0, 0, "c={:d}".format(c[p].item()), color='black',
-                            backgroundcolor='white', fontsize=8)
-                    plt.imshow(x[p].view(28, 28).cpu().data.numpy())
-                    plt.axis('off')
-
-                if not os.path.exists(os.path.join(args.fig_root, str(ts))):
-                    if not (os.path.exists(os.path.join(args.fig_root))):
-                        os.mkdir(os.path.join(args.fig_root))
-                    os.mkdir(os.path.join(args.fig_root, str(ts)))
-
-                plt.savefig(
-                    os.path.join(args.fig_root, str(ts),
-                                 "E{:d}I{:d}.png".format(epoch, iteration)),
-                    dpi=300)
-                plt.clf()
-                plt.close('all')
-
-        df = pd.DataFrame.from_dict(tracker_epoch, orient='index')
-        g = sns.lmplot(
-            x='x', y='y', hue='label', data=df.groupby('label').head(100),
-            fit_reg=False, legend=True)
-        g.savefig(os.path.join(
-            args.fig_root, str(ts), "E{:d}-Dist.png".format(epoch)),
-            dpi=300)
+    # 6 走到这里，说明多轮训练结束，保存模型（参数）
+    # 参1：模型对象的参数（权重矩阵，偏置矩阵） 参2：模型保存的文件名
+    print(f"\n保存的模型参数为：{vae.state_dict()}\n")
+    torch.save(vae.state_dict(), f"./model/CVAE.pth")  # 后缀名用:pth,pkl,pickle即可
 
 
 if __name__ == '__main__':
@@ -126,13 +90,15 @@ if __name__ == '__main__':
     parser.add_argument("--epochs", type=int, default=10)
     parser.add_argument("--batch_size", type=int, default=64)
     parser.add_argument("--learning_rate", type=float, default=0.001)
-    parser.add_argument("--encoder_layer_sizes", type=list, default=[784, 256])
-    parser.add_argument("--decoder_layer_sizes", type=list, default=[256, 784])
-    parser.add_argument("--latent_size", type=int, default=2)
+    # 输入：6个光学特性
+    parser.add_argument("--encoder_layer_sizes", type=list, default=[6, 128, 64])  # 6+6=12
+    # 条件：6个光学特性（作为条件） 输出：6个结构参数
+    parser.add_argument("--decoder_layer_sizes", type=list, default=[64, 128, 6])   # 输出6个参数
+    parser.add_argument("--latent_size", type=int, default=4)  # 增加潜在空间维度
     parser.add_argument("--print_every", type=int, default=100)
     parser.add_argument("--fig_root", type=str, default='figs')
-    parser.add_argument("--conditional", action='store_true')
+    parser.add_argument("--conditional", action='store_true', default=True)
 
-    args = parser.parse_args()
+    args = parser.parse_args() 
 
     main(args)
--- utils.py
+++ utils.py
@@ -1,12 +1,43 @@
-import torch
+# 导包
+import torch  # 封装了对张量的各种操作
+import numpy as np  # 对矩阵进行操作
+import pandas as pd  # 对数据集进行操作
+from torch.utils.data import TensorDataset  # 数据集对象 数据→Tensor→数据集→数据加载器
+from sklearn.model_selection import train_test_split  # 训练集和测试集的划分
+from sklearn.preprocessing import StandardScaler
 
 
-def idx2onehot(idx, n):
-    assert torch.max(idx).item() < n
+# 创建数据集
+def create_dataset():
+    # 1、加载csv文件数据集文件
+    data = pd.read_excel('./dataset/Δneff_comsol+网络.xlsx')
+    # 只使用前11545行数据，因为前面的数据是真的仿真数据，11545行后的数据是神经网络生成的数据
+    data = data.iloc[:11545, :-1]
 
-    if idx.dim() == 1:
-        idx = idx.unsqueeze(1)
-    onehot = torch.zeros(idx.size(0), n).to(idx.device)
-    onehot.scatter_(1, idx, 1)
+    # 2、获取x特征列和y标签列
+    # x：6个光学特性（作为输入）
+    # y：6个结构参数（作为输出）
+    x = data.iloc[:, 6:]  # 光学特性作为输入
+    y = data.iloc[:, :6]  # 结构参数作为输出
 
-    return onehot
+    # 3、把特征列转成浮点型，一定要转成浮点型，因为在神经网络中，计算都是浮点型
+    x = x.astype(np.float32)
+    y = y.astype(np.float32)
+
+    # 4、切分训练集和测试集
+    # 参1：特征 参2：标签 参3：测试集所占比例 参4：随机种子
+    x_train, x_test, y_train, y_test = train_test_split(x, y, test_size=0.2, random_state=32)
+
+    # 优化：数据标准化
+    transfer = StandardScaler()
+    x_train = transfer.fit_transform(x_train)
+    x_test = transfer.transform(x_test)
+    # 因为CVAE模型的输入是x和y，所以y也需要标准化
+    y_train = transfer.fit_transform(y_train)
+    y_test = transfer.transform(y_test)
+
+    # 5、把数据集封装成 张量数据集 思路：数据 → 张量Tensor → 数据集TensorDataSet → 数据加载器DataLoader
+    train_dataset = TensorDataset(torch.from_numpy(x_train), torch.from_numpy(y_train))
+    test_dataset = TensorDataset(torch.from_numpy(x_test), torch.from_numpy(y_test))
+
+    return train_dataset, test_dataset, x_train.shape[1], y_train.shape[1]
